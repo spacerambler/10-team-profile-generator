65371562d7693e8a167dcdfb11927571bc4ba7ed
Keely Bailey <keelybailey3@gmail.com>
2021-08-27 16:26:04 -0500

    Start cml prompts

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // purpose of this file is to run in terminal
+const inquirer = require('inquirer')
+const fs = require('fs')
+const util = require('util')
+
+const promptUser = () => {
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'manager-name',
+      message: 'Who is the team manager?',
+    }
+    {
+      type: 'input',
+      name: 'employee-id',
+      message: 'What is their employee ID?',
+    }
+    {
+      type: 'input',
+      name: 'email',
+      message: 'What is their email address?',
+    }
+    {
+      type: 'input',
+      name: 'office',
+      message: 'Which office are they in?',
+    }
+    {
+      type: '',
+      name: '',
+      message: '',
+    }
+  ])
+}

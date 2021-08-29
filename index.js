8dba54a0c71f1b0f95f2114c0be417144f256162
Keely Bailey <keelybailey3@gmail.com>
2021-08-29 09:48:03 -0500

    Try making inquirer loops

--- index.js
+++ index.js
@@ -32,3 +32,39 @@ const promptUser = () => {
     }
   ])
 }
+
+// want to try to use a for loop here inside the inquirer function
+
+inquirer
+.prompt([{
+  type: 'input',
+  name: 'manager-name',
+  message: 'Who is the team manager?',
+}
+{
+  type: 'input',
+  name: 'employee-id',
+  message: 'What is their employee ID?',
+}
+{
+  type: 'input',
+  name: 'email',
+  message: 'What is their email address?',
+}
+{
+  type: 'input',
+  name: 'office',
+  message: 'Which office are they in?',
+}])
+.next({
+  type: 'list',
+  name: 'roleSelection',
+  message: 'Select role to add next:',
+  choices: ['']
+})
+nextPrompt(function(){
+  if (input ==){} else {}
+})
+
+// self contained inquirer module
+
